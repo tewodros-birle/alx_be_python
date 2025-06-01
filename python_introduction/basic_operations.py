f25bf8c66fd0cf78a6f86173cdbbd220c7efbf47
tewodros-birle <tewodros.setargew@gmail.com>
2025-06-01 15:02:46 +0300

    Add basic_arithmetic exercise solution

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define the numbers
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
